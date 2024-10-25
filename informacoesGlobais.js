d8b2af5d308c1adee447e9dcc7976e5c3a70d8a6
marilcetrigueiro <110105677+marilcetrigueiro@users.noreply.github.com>
2024-10-25 15:22:09 -0300

    Add files via upload

---
+++ informacoesGlobais.js
@@ -0,0 +1,12 @@
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
+async function vizualizarInformacoesGlobais() {
+    const res =await fetch(url)
+    const dados = await res.json()
+    console.log(dados);
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container_texto')
+    paragrafo.innerHTML = 'você sabia que o mundo tem ${dados.total_}'
+}
+
+visualizarInformacoesGlobais()
